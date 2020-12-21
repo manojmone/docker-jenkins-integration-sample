931fc234a3bb8400a7ba2c4b983213f394e612ac
Manoj Mone <manojmone@gmail.com>
2020-12-21 18:54:07 +0530

    Checking demo flow with Prakash

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello there from Manoj");
+        System.out.println("Hello there from Manoj and Prakash");
     }
 }
