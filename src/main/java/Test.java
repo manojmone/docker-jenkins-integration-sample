3882632aca222afe9a57be7c2b8086a7a58137cc
Manoj Mone <manojmone@gmail.com>
2020-12-22 06:55:47 +0530

    Initiating CI/CD and Git Sync

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("DevOps Demo using Jenkins");
+        System.out.println("DevOps Demo using Jenkins Pipeline");
     }
 }
