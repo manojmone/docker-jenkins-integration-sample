0491fe60b6476d732778637c891b0a5af428f0c1
Manoj Mone <manojmone@gmail.com>
2020-12-22 06:26:20 +0530

    Initiating Maven Demo

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello there from Manoj and Prakash");
+        System.out.println("DevOps Demo using Jenkins");
     }
 }
