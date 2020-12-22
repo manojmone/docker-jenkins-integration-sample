822fbdea259c742e4db07784d89d4d73a1965f53
Manoj Mone <manojmone@gmail.com>
2020-12-22 09:31:10 +0530

    Demo to SEs for Git

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("DevOps Demo using Jenkins Pipeline");
+        System.out.println("DevOps Demo to SEs to show the use of Jenkins Pipeline and Docker");
     }
 }
